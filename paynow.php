f967312768b3d13888038fae241c074a21d47999
salonigharge <salonigharge035@gmail.com>
2020-08-12 17:33:46 +0530

    please work

--- paynow.php
+++ paynow.php
@@ -6,29 +6,34 @@ $Phone=$_POST['telnum'];
 $Amount=$_POST['amount'];
 $purpose='Donation';
 
-include 'Instamojo/Instamojo.php';
-$api = new Instamojo\Instamojo('test_50e73d7b55322908f181f93f97e', 'test_34226f83ae8ec3a27258514181b', 'https://test.instamojo.com/api/1.1/');
+<?php
 
-try {
-    $response = $api->paymentRequestCreate(array(
-        "purpose" => $purpose,
-        "amount" => $Amount,
-        "send_email" => true,
-        "email" => $Email,
-        "buyer_name" =>$Name,
-        "phone"=>$Phone,
-        "send_sms" => true,
-        "allow_repeated_payments" =>false,
-        "redirect_url" => "https://salonigharge.github.io/Donationgateway/redirect.php"
-        ));
-    //print_r($response);
-    $json_decode=json_decode(response,true);
-    print($json_decode);
-    $long_url=$json_decode['payment_request']['longurl'];
-   // header("location: $long_url");
-	}
-	catch (Exception $e) {
-	    print('Error: ' . $e->getMessage());
-	}
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:test_50e73d7b55322908f181f93f97e",
+                  "X-Auth-Token:test_34226f83ae8ec3a27258514181b"));
+$payload = Array(
+    'purpose' => '$purpose',
+    'amount' => '$Amount',
+    'phone' => '$phone',
+    'buyer_name' => '$Name',
+    'redirect_url' => 'https://salonigharge.github.io/Donationgateway/redirect.php',
+    'send_email' => true,
+    'webhook' => '',
+    'send_sms' => false,
+    'email' => '$Email',
+    'allow_repeated_payments' => false
+);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
 
 ?>
