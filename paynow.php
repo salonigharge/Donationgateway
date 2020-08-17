7887f0f4a3e9d0a041015678aad0c29e151c14ca
salonigharge <salonigharge035@gmail.com>
2020-08-17 23:18:25 +0530

    123

--- paynow.php
+++ paynow.php
@@ -1,32 +1,39 @@
-
 <?php
 $Name=$_POST['name'];
 $Email=$_POST['email'];
 $Amount=$_POST['amount'];
 $Phone=$_POST['telnum'];
-$purpose='Donation';
 
-include 'Instamojo/Instamojo.php';
-$api = new Instamojo\Instamojo('test_50e73d7b55322908f181f93f97e', 'test_34226f83ae8ec3a27258514181b', 'https://test.instamojo.com/api/1.1/');
+$ch = curl_init();
 
-try {
-    $response = $api->paymentRequestCreate(array(
-        "purpose" => $purpose,
-        "amount" => $Amount,
-        "send_email" => true,
-        "email" => $Email,
-        "buyer_name" =>$Name,
-        "phone"=>$Phone,
-        "send_sms" => true,
-        "allow_repeated_payments" =>false,
-        "redirect_url" => "https://donationgateway.herokuapp.com/redirect.php"
-        ));
-    //print_r($response);
-    $pay_url=$response['longurl'];
-    header("location: $pay_url");
-    }
-    catch (Exception $e) {
-        print('Error: ' . $e->getMessage());
-    }
+curl_setopt($ch, CURLOPT_URL, 'https://test.instamojo.com/api/1.1/payment-requests/');
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array("X-Api-Key:test_50e73d7b55322908f181f93f97e",
+                  "X-Auth-Token:test_34226f83ae8ec3a27258514181b"));
+$payload = Array(
+    'purpose' => 'Donation',
+    'amount' => $Amount,
+    'phone' => $Phone,
+    'buyer_name' => $Name,
+    'redirect_url' => 'https://donationgateway.herokuapp.com/redirect.php',
+    'send_email' => true,
+    'webhook' => '',
+    'send_sms' => true,
+    'email' => $Email,
+    'allow_repeated_payments' => false
+);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
+$response = curl_exec($ch);
+curl_close($ch); 
+
+echo $response;
 
 ?>
+
+
+
+
