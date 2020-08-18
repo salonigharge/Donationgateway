5d20cff3ffb0653967c2ef2f5b449bc249ad205c
salonigharge <salonigharge035@gmail.com>
2020-08-18 20:01:37 +0530

    hello

--- webhook.php
+++ webhook.php
@@ -1,4 +1,9 @@
 <?php
-  echo $_GET['payment_id'].'<br>';
-  echo $_GET['payment_request_id'].'<br>';
+require_once 'pdo.php';
+require 'connet.php';
+ $data= $_POST ;
+ $payment_id=$data['payment_id'];
+ $payment_request_id=$data['payment_request_id'];
+ mysql_query("INSERT INTO webhook ('imojo_id','payment_id','status') VALUES ('$payment_id','payment_request_id','')");
+
 ?>
